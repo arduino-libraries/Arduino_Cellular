0db0da0b0d993fd855f123dad197573cc31da631
Sebastian Romero <s.romero@arduino.cc>
2024-04-11 09:34:06 +0200

    Remove test data

--- examples/ReceiveSMS/arduino_secrets.h
+++ examples/ReceiveSMS/arduino_secrets.h
@@ -2,9 +2,3 @@
 #define SECRET_GPRS_APN      "apn" // replace with your GPRS APN
 #define SECRET_GPRS_LOGIN    "login"    // replace with your GPRS login
 #define SECRET_GPRS_PASSWORD "password" // replace with your GPRS password
-
-
-#define SECRET_PINNUMBER     "1234" // replace with your SIM card PIN
-#define SECRET_GPRS_APN      "live.vodafone.com" // replace with your GPRS APN
-#define SECRET_GPRS_LOGIN    "live"    // replace with your GPRS login
-#define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
