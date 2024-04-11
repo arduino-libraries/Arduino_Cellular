0b32193ffb90acd71d5f53df09f4b05e314f779a
Sebastian Romero <s.romero@arduino.cc>
2024-04-11 09:37:17 +0200

    Add polling to SMS receive example

--- examples/ReceiveSMS/ReceiveSMS.ino
+++ examples/ReceiveSMS/ReceiveSMS.ino
@@ -1,24 +1,24 @@
 #include "ArduinoCellular.h"
 #include "arduino_secrets.h"
 
-// #define TINY_GSM_DEBUG Serial
-// #define ARDUINO_CELLULAR_DEBUG
 
 constexpr int NEW_SMS_INTERRUPT_PIN = A0;
 
 ArduinoCellular cellular = ArduinoCellular();
 volatile boolean smsReceived = false;
+constexpr int POLLING_INTERVAL_MS = 1 * 60 * 1000; // 1 minute
 
 void printMessages(std::vector<SMS> msg){
      for(int i = 0; i < msg.size(); i++){
         Serial.println("SMS:");
-        Serial.print("\t * From: "); Serial.println(msg[i].number);
-        Serial.print("\t * Message: "); Serial.println(msg[i].message);
-        Serial.print("\t * Timestamp: "); Serial.println(msg[i].timestamp.getISO8601());
+        Serial.print("* Index: "); Serial.println(msg[i].index);
+        Serial.print("* From: "); Serial.println(msg[i].sender);
+        Serial.print("* Timestamp: "); Serial.println(msg[i].timestamp.getISO8601());
+        Serial.println("* Message: "); Serial.println(msg[i].message);
+        Serial.println("--------------------\n");
     }
 }
 void onSMSReceived(){
-    Serial.println("New SMS received!");
     smsReceived = true;
 }
 
@@ -26,28 +26,43 @@ void setup(){
     Serial.begin(115200);
     while (!Serial);
     cellular.setDebugStream(Serial);
-
     cellular.begin();
+    
     Serial.println("Connecting...");
     cellular.connect(SECRET_GPRS_APN, SECRET_GPRS_LOGIN, SECRET_GPRS_PASSWORD, SECRET_PINNUMBER);
-    
+
     // Register interrupt based callback for new SMS
     attachInterrupt(digitalPinToInterrupt(NEW_SMS_INTERRUPT_PIN), onSMSReceived, RISING);
 
-    Serial.println("Read SMS:");
     std::vector<SMS> readSMS = cellular.getReadSMS();
-    printMessages(readSMS);
+    if(readSMS.size() > 0){
+        Serial.println("Read SMS:");
+        printMessages(readSMS);
+    }
 
-    Serial.println("Unread SMS:");
     std::vector<SMS> unreadSMS = cellular.getUnreadSMS();
-    printMessages(unreadSMS);
+    if(unreadSMS.size() > 0){
+        Serial.println("Unread SMS:");
+        printMessages(unreadSMS);
+    }
 }
 
 void loop(){
-    if(smsReceived){
+    static unsigned long lastPoll = 0;
+    static bool checkForNewSMS = false;
+
+    if(millis() - lastPoll > POLLING_INTERVAL_MS){
+        checkForNewSMS = true;
+        lastPoll = millis();
+    }
+
+    if(smsReceived || checkForNewSMS){
         smsReceived = false;
+        checkForNewSMS = false;
         std::vector<SMS> unreadSMS = cellular.getUnreadSMS();
+
         if (unreadSMS.size() > 0){
+            Serial.println("New SMS received!");
             printMessages(unreadSMS);
         }
     }
